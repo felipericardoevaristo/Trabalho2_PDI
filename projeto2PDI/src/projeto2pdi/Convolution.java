62ea20d2c5e958b596f3f5176da3740fd5dda720
felipericardoevaristo <felipetuning18@hotmail.com>
2016-08-10 16:16:21 -0300

    Questão 2.E do 2 trabalho esta OK

--- projeto2PDI/src/projeto2pdi/Convolution.java
+++ projeto2PDI/src/projeto2pdi/Convolution.java
@@ -615,95 +615,40 @@ public class Convolution {
     }
     
     //media
-    public static BufferedImage convMedia(BufferedImage img,double M,double N) {
-        double[][] h1= new double[3][3];
-        h1[0][0]= (double)1/(M*N);
-        h1[0][1]= (double)1/(M*N);
-        h1[0][2]= (double)1/(M*N);
-        
-        h1[1][0]= (double)1/(M*N);
-        h1[1][1]= (double)1/(M*N);
-        h1[1][2]= (double)1/(M*N);
-        
-        h1[2][0]= (double)1/(M*N);
-        h1[2][1]= (double)1/(M*N);
-        h1[2][2]= (double)1/(M*N);
-        
-        
-        
-        int largura = img.getWidth();
-        int altura = img.getHeight();
-        System.out.println("Domensoes: "+largura+"x"+altura);     // Debugando
-        BufferedImage nova  = new BufferedImage(largura, altura, 1);
-        
-        double somaR, somaG, somaB;
-        
-        for (int i = 0;i < largura; i++) {
-            for (int j = 0; j < altura; j++) {
-                somaR = 0;
-                somaG = 0;
-                somaB = 0;
-                int indice = 0, r1=0, g1=0, b1=0, p=0;
-                
-                for(int a=i-1; a<=i+1 ;a++,p++){
-                    int q=0;
-                    for(int b=j-1; b<=j+1 ;b++,q++){
+    public static BufferedImage convMedia(BufferedImage img,int M,int N) {
+        int width = img.getWidth(); //Largura da imagem
+        int height = img.getHeight(); //Altura da imagem
+        int r, g, b, //Componentes de cor
+            i2=0, j2=0; //Variáveis auxiliares, servem para varrer o pixel central e seus vizinhos
+        Color rgb; //Cor a ser setada na imagem
 
-                        int[] rebatimento = Calculos.rebatimento(a,b,largura,altura);
-                        int m = rebatimento[0];
-                        int n = rebatimento[1];
-                        int rgb1 = img.getRGB(m,n);
-
-                        r1 = ((rgb1&0x00FF0000)>>>16);
-                        g1 = ((rgb1&0x0000FF00)>>> 8);
-                        b1 = (rgb1& 0x000000FF);
-                        somaR += r1 * h1[p][q];
-                        somaG += g1 * h1[p][q];
-                        somaB += b1 * h1[p][q];
+        //Varre a imagem inteira
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                //A cada pixel visitado, zera os elementos de cor para não usar lixo da iteração anterior
+                r = g = b = 0;
+                //Varre o elemento do meio e seus vizinhos
+                for(i2=i-(M/2); i2 <= i+(M/2); i2++){
+                    for(j2=j-(N/2); j2 <= j+(N/2); j2++){
+                        //Se algum dos vizinhos estiver fora dos limites da imagem, suas componentes adicionarão 0, ou simplemente não haverá processamento
+                        try{
+                            r += new Color(img.getRGB(i2, j2)).getRed();
+                            g += new Color(img.getRGB(i2, j2)).getGreen();
+                            b += new Color(img.getRGB(i2, j2)).getBlue();
+                        }
+                        catch(ArrayIndexOutOfBoundsException e){
+                            //r += 0; g += 0; b += 0; // Processamento desnecessário
+                        }
                     }
-                }
-                if(somaR>255){
-                    somaR = 255;
-                }
-                if(somaR<0){
-                    somaR = 0;
-                }
-                if(somaG>255){
-                    somaG = 255;
-                }
-                if(somaG<0){
-                    somaG = 0;
-                }
-                if(somaB>255){
-                    somaB = 255;
-                }
-                if(somaB<0){
-                    somaB = 0;
-                }
-
-                Color color = new Color( (int)somaR, (int)somaG, (int)somaB);
-                nova.setRGB(i, j, color.getRGB());
-            }
-        }
-        
-        
-        //Gambiarra para nao mexer na imagem original
-        for (int i = 0;i < largura; i++) {
-            for (int j = 0; j < altura; j++) {
-                
-                int indice = 0, r1=0, g1=0, b1=0, p=0;
-                
-                            int rgb1 = nova.getRGB(i, j);
-                            r1 = ((rgb1&0x00FF0000)>>>16);
-                            g1 = ((rgb1&0x0000FF00)>>> 8);
-                            b1 = (rgb1 & 0xFF);
-
-                Color color = new Color(r1,g1,b1);
-                img.setRGB(i, j, color.getRGB());
+                }           
+                r /= (M*N); g /= (M*N); b /= (M*N); //Após a soma, calcula-se a média
+                rgb = new Color(r, g, b);
+                img.setRGB(i, j, rgb.getRGB()); //Insere a cor no pixel do meio
             }
         }
         return img;
     }
+        
     
     //borda 2
     public static BufferedImage convBordas2(BufferedImage img) {
@@ -795,6 +740,7 @@ public class Convolution {
         return img;
     }
    
+    
     //borda 3
     public static BufferedImage convBordas3(BufferedImage img) {
         double[][] h1= new double[3][3];
